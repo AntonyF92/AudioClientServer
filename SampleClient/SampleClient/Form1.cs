68c07ad94464cdef89f357b70a34866bd093d4bc
Antony <asfalot92@gmail.com>
2014-11-17 17:49:33 +0300

    remove useless scripts

--- SampleClient/SampleClient/Form1.cs
+++ SampleClient/SampleClient/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MediaServer;
 using System.Net;
 using System.Net.Sockets;
 using System.Web;
