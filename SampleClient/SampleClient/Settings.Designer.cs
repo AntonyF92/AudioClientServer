5db8a5ccb9cc36bfbce142b6cb08021d010af66d
Antony <asfalot92@gmail.com>
2014-11-20 11:09:07 +0300

    settings

---
+++ SampleClient/SampleClient/Settings.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace SampleClient
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ServerList = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TcpPort = new System.Windows.Forms.TextBox();
+            this.HttpPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BufferSize = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.MinimizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.MinimizeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinimizeButton.Location = new System.Drawing.Point(295, 2);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(33, 23);
+            this.MinimizeButton.TabIndex = 17;
+            this.MinimizeButton.Text = "__";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CloseButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Image = global::SampleClient.Properties.Resources._1416407871_ic_close_48px_24;
+            this.CloseButton.Location = new System.Drawing.Point(334, 3);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 23);
+            this.CloseButton.TabIndex = 18;
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Application settings";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Audio server address";
+            // 
+            // ServerList
+            // 
+            this.ServerList.FormattingEnabled = true;
+            this.ServerList.Location = new System.Drawing.Point(12, 58);
+            this.ServerList.Name = "ServerList";
+            this.ServerList.Size = new System.Drawing.Size(227, 21);
+            this.ServerList.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "TCP port";
+            // 
+            // TcpPort
+            // 
+            this.TcpPort.Location = new System.Drawing.Point(253, 58);
+            this.TcpPort.Name = "TcpPort";
+            this.TcpPort.Size = new System.Drawing.Size(100, 20);
+            this.TcpPort.TabIndex = 23;
+            this.TcpPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TcpPort_KeyPress);
+            // 
+            // HttpPort
+            // 
+            this.HttpPort.Location = new System.Drawing.Point(12, 101);
+            this.HttpPort.Name = "HttpPort";
+            this.HttpPort.Size = new System.Drawing.Size(100, 20);
+            this.HttpPort.TabIndex = 25;
+            this.HttpPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TcpPort_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Http port";
+            // 
+            // BufferSize
+            // 
+            this.BufferSize.Location = new System.Drawing.Point(139, 101);
+            this.BufferSize.Name = "BufferSize";
+            this.BufferSize.Size = new System.Drawing.Size(100, 20);
+            this.BufferSize.TabIndex = 27;
+            this.BufferSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TcpPort_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Audio buffer size";
+            // 
+            // OK
+            // 
+            this.OK.BackColor = System.Drawing.Color.SteelBlue;
+            this.OK.FlatAppearance.BorderSize = 0;
+            this.OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OK.Location = new System.Drawing.Point(315, 154);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(49, 23);
+            this.OK.TabIndex = 28;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = false;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(365, 179);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.BufferSize);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.HttpPort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TcpPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ServerList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.MinimizeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ServerList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TcpPort;
+        private System.Windows.Forms.TextBox HttpPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BufferSize;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button OK;
+    }
+}
---
+++ SampleClient/SampleClient/Settings.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleClient
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            foreach (string host in ConfigManager.Instance.config.possible_hosts)
+                ServerList.Items.Add(host);
+            if (!ConfigManager.Instance.config.possible_hosts.Contains(ConfigManager.Instance.config.audio_server_dns))
+                ServerList.Items.Add(ConfigManager.Instance.config.audio_server_dns);
+            ServerList.SelectedItem = ConfigManager.Instance.config.audio_server_dns;
+            TcpPort.Text = ConfigManager.Instance.config.audio_port.ToString();
+            HttpPort.Text = ConfigManager.Instance.config.http_port.ToString();
+            BufferSize.Text = ConfigManager.Instance.config.audio_buffer_size.ToString();
+        }
+
+        private void TcpPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = false;
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(ServerList.Text))
+                    throw new ArgumentNullException("Audio server address");
+                if (string.IsNullOrEmpty(HttpPort.Text))
+                    throw new ArgumentNullException("Http port");
+                if (string.IsNullOrEmpty(TcpPort.Text))
+                    throw new ArgumentNullException("TCP port");
+                if (string.IsNullOrEmpty(BufferSize.Text))
+                    throw new ArgumentNullException("Buffer size");
+                ConfigManager.Instance.SetHost(ServerList.Text);
+                ConfigManager.Instance.SetIntOption(ConfigFields.http_port, HttpPort.Text);
+                ConfigManager.Instance.SetIntOption(ConfigFields.audio_port, TcpPort.Text);
+                ConfigManager.Instance.SetIntOption(ConfigFields.audio_buffer_size, BufferSize.Text);
+                ConfigManager.Instance.SaveConfig();
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        int iFormX, iFormY, iMouseX, iMouseY;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            iFormX = this.Location.X;
+            iFormY = this.Location.Y;
+            iMouseX = MousePosition.X;
+            iMouseY = MousePosition.Y;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int iMouseX2 = MousePosition.X;
+            int iMouseY2 = MousePosition.Y;
+            if (e.Button == MouseButtons.Left)
+                this.Location = new Point(iFormX + (iMouseX2 - iMouseX), iFormY + (iMouseY2 - iMouseY));
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
