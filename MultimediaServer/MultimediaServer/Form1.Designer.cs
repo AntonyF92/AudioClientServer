73d09a559a41800e15b1496799ed71ce5b5036fa
Antony <asfalot92@gmail.com>
2014-11-20 16:58:04 +0300

    new server interface begining

--- MultimediaServer/MultimediaServer/Form1.Designer.cs
+++ MultimediaServer/MultimediaServer/Form1.Designer.cs
@@ -39,6 +39,10 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.CloseButton = new System.Windows.Forms.Button();
             this.MinimizeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlaylistCollectionWindow = new System.Windows.Forms.TabControl();
+            this.AddPlaylist = new System.Windows.Forms.Button();
+            this.RemovePlaylist = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -55,10 +59,11 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.PlaylistCollectionWindow);
             this.panel1.Controls.Add(this.menuStrip1);
-            this.panel1.Location = new System.Drawing.Point(12, 46);
+            this.panel1.Location = new System.Drawing.Point(12, 30);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(517, 451);
+            this.panel1.Size = new System.Drawing.Size(392, 467);
             this.panel1.TabIndex = 0;
             // 
             // menuStrip1
@@ -68,7 +73,7 @@
             this.serverToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(517, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(392, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -108,7 +113,7 @@
             this.CloseButton.FlatAppearance.BorderSize = 0;
             this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CloseButton.Image = global::MediaServer.Properties.Resources._1416407871_ic_close_48px_24;
-            this.CloseButton.Location = new System.Drawing.Point(510, 1);
+            this.CloseButton.Location = new System.Drawing.Point(374, 4);
             this.CloseButton.Name = "CloseButton";
             this.CloseButton.Size = new System.Drawing.Size(30, 23);
             this.CloseButton.TabIndex = 18;
@@ -122,19 +127,59 @@
             this.MinimizeButton.FlatAppearance.BorderSize = 0;
             this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.MinimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MinimizeButton.Location = new System.Drawing.Point(471, 1);
+            this.MinimizeButton.Location = new System.Drawing.Point(335, 4);
             this.MinimizeButton.Name = "MinimizeButton";
             this.MinimizeButton.Size = new System.Drawing.Size(33, 23);
             this.MinimizeButton.TabIndex = 17;
             this.MinimizeButton.Text = "__";
             this.MinimizeButton.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Audio server";
+            // 
+            // PlaylistCollectionWindow
+            // 
+            this.PlaylistCollectionWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PlaylistCollectionWindow.Location = new System.Drawing.Point(0, 24);
+            this.PlaylistCollectionWindow.Name = "PlaylistCollectionWindow";
+            this.PlaylistCollectionWindow.SelectedIndex = 0;
+            this.PlaylistCollectionWindow.Size = new System.Drawing.Size(392, 443);
+            this.PlaylistCollectionWindow.TabIndex = 12;
+            // 
+            // AddPlaylist
+            // 
+            this.AddPlaylist.Location = new System.Drawing.Point(12, 503);
+            this.AddPlaylist.Name = "AddPlaylist";
+            this.AddPlaylist.Size = new System.Drawing.Size(75, 23);
+            this.AddPlaylist.TabIndex = 20;
+            this.AddPlaylist.Text = "Add playlist";
+            this.AddPlaylist.UseVisualStyleBackColor = true;
+            // 
+            // RemovePlaylist
+            // 
+            this.RemovePlaylist.Location = new System.Drawing.Point(93, 503);
+            this.RemovePlaylist.Name = "RemovePlaylist";
+            this.RemovePlaylist.Size = new System.Drawing.Size(89, 23);
+            this.RemovePlaylist.TabIndex = 21;
+            this.RemovePlaylist.Text = "Remove playlist";
+            this.RemovePlaylist.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(541, 509);
+            this.ClientSize = new System.Drawing.Size(410, 538);
+            this.Controls.Add(this.RemovePlaylist);
+            this.Controls.Add(this.AddPlaylist);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.MinimizeButton);
             this.Controls.Add(this.panel1);
@@ -150,6 +195,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -165,6 +211,10 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.TabControl PlaylistCollectionWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddPlaylist;
+        private System.Windows.Forms.Button RemovePlaylist;
     }
 }
 
