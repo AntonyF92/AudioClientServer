eb39edf7e9e8077b7eae80f4d41aef42f57f1871
Antony <asfalot92@gmail.com>
2014-11-21 17:56:32 +0300

    input box

---
+++ MultimediaServer/MultimediaServer/InputBox.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace MediaServer
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ValueBox = new System.Windows.Forms.TextBox();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Enabled = false;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Title.Location = new System.Drawing.Point(12, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(46, 18);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "label1";
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.MinimizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.MinimizeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinimizeButton.Location = new System.Drawing.Point(336, -1);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(33, 23);
+            this.MinimizeButton.TabIndex = 19;
+            this.MinimizeButton.TabStop = false;
+            this.MinimizeButton.Text = "__";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CloseButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Image = global::MediaServer.Properties.Resources._1416407871_ic_close_48px_24;
+            this.CloseButton.Location = new System.Drawing.Point(375, -1);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 23);
+            this.CloseButton.TabIndex = 20;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // ValueBox
+            // 
+            this.ValueBox.Location = new System.Drawing.Point(15, 63);
+            this.ValueBox.Name = "ValueBox";
+            this.ValueBox.Size = new System.Drawing.Size(380, 20);
+            this.ValueBox.TabIndex = 1;
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.FlatAppearance.BorderSize = 0;
+            this.Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cancel.Location = new System.Drawing.Point(320, 91);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 21;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // OK
+            // 
+            this.OK.FlatAppearance.BorderSize = 0;
+            this.OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OK.Location = new System.Drawing.Point(273, 91);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(41, 23);
+            this.OK.TabIndex = 22;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(407, 126);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.ValueBox);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.MinimizeButton);
+            this.Controls.Add(this.Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InputBox";
+            this.Text = "InputBox";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.TextBox ValueBox;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button OK;
+    }
+}
---
+++ MultimediaServer/MultimediaServer/InputBox.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaServer
+{
+    public partial class InputBox : Form
+    {
+        string value = "";
+
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        int iFormX, iFormY, iMouseX, iMouseY;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            iFormX = this.Location.X;
+            iFormY = this.Location.Y;
+            iMouseX = MousePosition.X;
+            iMouseY = MousePosition.Y;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int iMouseX2 = MousePosition.X;
+            int iMouseY2 = MousePosition.Y;
+            if (e.Button == MouseButtons.Left)
+                this.Location = new Point(iFormX + (iMouseX2 - iMouseX), iFormY + (iMouseY2 - iMouseY));
+
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            value = ValueBox.Text;
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        public static DialogResult ShowBox(string title, out string value)
+        {
+            InputBox box = new InputBox();
+            box.Title.Text = title;
+            var result = box.ShowDialog();
+            value = box.ValueBox.Text;
+            box.Dispose();
+            return result;
+        }
+    }
+}
